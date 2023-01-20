f4c81317cff6ddab9966173bd6ae5a0ff339f826
NaragudeNitin <naragudenitin@gmail.com>
2023-01-20 15:45:07 +0530

    local notification

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'set_notifi.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: const SetNotification(title: 'Set Notification'),
+    );
+  }
+}
+
